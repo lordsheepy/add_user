1ddfa9073c6476140c77c3e2e23c7f3deffb1740
Stephen Babineau <stephen.babineau@gmail.com>
2014-01-31 15:12:49 -0700

    added function to detail.html

--- add_user/urls.py
+++ add_user/urls.py
@@ -4,6 +4,6 @@ from django.contrib import admin
 admin.autodiscover()
 
 urlpatterns = patterns('',
-    url(r'^adduser/', include('adduser.urls')),
+    url(r'^adduser/', include('adduser.urls', namespace="adduser")),
     url(r'^admin/', include(admin.site.urls)),
 )
--- adduser/views.py
+++ adduser/views.py
@@ -1,12 +1,14 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse
 from adduser.models import User
 
 def index(request):
-    user_list = User.objects.all()
-    return render(request, 'adduser/index.html', user_list)
+    user_list = User.objects.all().order_by('last_name')
+    context = {'user_list': user_list}
+    return render(request, 'adduser/index.html', context)
 
 def detail(request, user_id):
-    return HttpResponse("You're looking at the details for %s." %user_id)
+    userdetail = get_object_or_404(User, pk=user_id)
+    return render(request, 'adduser/detail.html', {'userdetail': userdetail})
 
 
