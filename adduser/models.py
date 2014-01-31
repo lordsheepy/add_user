5495a2174c98a82251d5705b0db6f5a287e82f0e
Stephen Babineau <stephen.babineau@gmail.com>
2014-01-31 13:52:15 -0700

    1/31/14 13:52

--- adduser/models.py
+++ adduser/models.py
@@ -2,6 +2,11 @@
 from django.db import models
 
 class User(models.Model):
-    first_name = models.CharField(max_length=50)
-    last_name = models.CharField(max_length=50)
-    email = models.EmailField(max_length=200)
+    first_name = models.CharField(max_length=50,
+                                  blank=False)
+    last_name = models.CharField(max_length=50,
+                                 blank=True)
+    email = models.EmailField(max_length=200,
+                              blank=True)
+    def __str__(self):
+        return self.first_name
--- adduser/views.py
+++ adduser/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from adduser.models import User
 
-# Create your views here.
+def index(request):
+    user_list = User.objects.all()
+    return render(request, 'adduser/index.html', user_list)
+
+def detail(request, user_id):
+    return HttpResponse("You're looking at the details for %s." %user_id)
+
+
