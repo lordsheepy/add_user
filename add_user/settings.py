d7fc05af867b27b222d417ef6dceb776afd71a05
Stephen Babineau <stephen.babineau@gmail.com>
2014-02-02 15:34:44 -0700

    Procfile correction

--- add_user/settings.py
+++ add_user/settings.py
@@ -57,8 +57,11 @@ WSGI_APPLICATION = 'add_user.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'db_adduser',
+        'USER': 'db_user',
+        'PASSWORD': '',
+        'HOST': ''
     }
 }
 
