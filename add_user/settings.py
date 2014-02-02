c8f82dcb400ac530ca9a8ddc430eb9c212f05f3a
Stephen Babineau <stephen.babineau@gmail.com>
2014-02-02 16:08:06 -0700

    Procfile correction

--- add_user/settings.py
+++ add_user/settings.py
@@ -20,9 +20,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(__file__))
 SECRET_KEY = 'exb4@g@xqjf9$e4b)$ggqozx*@y=0!97v2035cs!704*mlpn-f'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-TEMPLATE_DEBUG = True
+TEMPLATE_DEBUG = False
 
 ALLOWED_HOSTS = []
 
